311d66e9dac111536c2e09764bb0ffa85e8b5f11
Johnatan Illi <johnatan.illi@exoscale.ch>
2020-05-28 16:04:47 +0200

    add github action & unit test

--- Makefile
+++ Makefile
@@ -1,3 +1,7 @@
 include go.mk/init.mk
 
-GO_BIN_OUTPUT_NAME := exoscale-import
+EXTRA_ARGS := -parallel 3 -count=1 -failfast
+
+.PHONY: test-acc test-verbose test
+test: GO_TEST_EXTRA_ARGS=${EXTRA_ARGS}
+test-verbose: GO_TEST_EXTRA_ARGS+=$(EXTRA_ARGS)
---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+
+	exoscaleimport "github.com/exoscale/packer-post-processor-exoscale-import/exoscale-import"
+	"github.com/hashicorp/packer/packer/plugin"
+)
+
+func main() {
+	server, err := plugin.Server()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := server.RegisterPostProcessor(new(exoscaleimport.PostProcessor)); err != nil {
+		log.Fatal(err)
+	}
+
+	server.Serve()
+}
